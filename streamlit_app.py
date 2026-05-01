3f0fe233e31d3f5766188df931f5ff6b860720b9
Victor Leung <victorleungtw@gmail.com>
2026-05-01 14:30:54 +0800

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,55 @@
+# Import python packages.
+import streamlit as st
+from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col
+
+# Write directly to the app.
+st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
+st.write(
+  """Choose the fruits you want in your custom Smoothie!
+  """
+)
+
+name_on_order = st.text_input("Name on Smoothie:")
+st.write("The name on your Smoothie will be:", name_on_order)
+
+session = get_active_session()
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+# st.dataframe(data=my_dataframe, use_container_width=True)
+
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
+
+ingredients_list = st.multiselect(
+    'Choose up to 5 ingredients',
+    my_dataframe,
+    max_selection=5
+)
+
+if ingredients_list:
+
+    ingredients_string = ''
+
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ' '
+
+    search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+    # st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+
+    # st.write(ingredients_string)
+
+    st.subheader(fruit_chosen + ' Nutrition Information')
+    smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on}")
+    sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+    
+    # Fixed statement adding the missing column name
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+            values ('""" + ingredients_string + """','""" + name_on_order + """')"""
+
+    time_to_insert = st.button('Submit Order')
+
+    if time_to_insert:
+        session.sql(my_insert_stmt).collect()
+        
+        st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
